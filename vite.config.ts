7e43ca47dfa77cd2a6d48ed2c035de30828c14a0
ckojak <bmw.reta@hotmail.com>
2025-12-06 18:45:03 -0300

    Enhance build config for chunk size optimization
    Added build configuration to optimize chunk sizes.

--- vite.config.ts
+++ vite.config.ts
@@ -14,4 +14,18 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  // ADICIONADO: Configuração de Build para otimizar o tamanho dos chunks
+  build: {
+    chunkSizeWarningLimit: 1600, // Aumenta o limite de aviso para 1.6MB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Esta lógica separa as bibliotecas do node_modules em chunks menores
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
 });
